a0eda7ad75398dbbbd4abddd940d58ce5bdd8d8f
John McKeown <jackamckeown@gmail.com>
2016-02-28 22:58:43 -0600

    Fixed it to work with SQLite!

--- server.py
+++ server.py
@@ -1,41 +1,40 @@
+#!/usr/bin/python
 import socket
 import re
 import urllib
-import pickle
+#import pickle
+import sqlite3
+#from urllib.parse import urlparse
 
 values = {}
-HOST, PORT, REQUEST_SIZE, DICTIONARY_FILE = '', 8888, 4096, "SimpleSave.dat"
+HOST, PORT, REQUEST_SIZE, DICTIONARY_FILE = '', 8888, 4096, "SimpleSave.db"
+HTTP_200 = """HTTP/1.1 200 OK \n\n"""
 
 def main():
     listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     listen_socket.bind((HOST, PORT))
     listen_socket.listen(1)
+
+    sqlConnection = sqlite3.connect(DICTIONARY_FILE)
+    cursor = sqlConnection.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS val (key text, val text);");
+    sqlConnection.commit()
+    sqlConnection.close()
     
-    try:
-        with open(DICTIONARY_FILE, 'rb') as handle:
-            try:
-                global values
-                values = pickle.loads(handle.read())
-            except:
-                pass
-            handle.close()
-    except:
-        pass
-    
-    print values
     while True:
         client_connection, client_address = listen_socket.accept()
         request = client_connection.recv(REQUEST_SIZE)
-        http_response = """HTTP/1.1 200 OK \n\n"""+handleRequest(request)
+        http_response = HTTP_200 + handleRequest(request)
         client_connection.sendall(http_response)
         client_connection.close()
 
 def handleRequest(request):
-    request = request.split(' ')[1] #remove extra HTTP headers and get only URL data
+    request = request.split(' ')[1] #remove extra HTTP headers and get only the URL data
     request = re.split("&|\?",request)
     key = False
     value = False
+
     for i in request:
 	if i[0:4] == "key=":
             key = urllib.unquote(i[4:])
@@ -43,24 +42,47 @@ def handleRequest(request):
 	    value = urllib.unquote(i[6:])
     if isinstance(key,str):
 	if isinstance(value,str):
-	    setValue(key,value)
+	    val = setValue(key,value)
+            if(isinstance(val,str)):
+                return val
+            else:
+                return "" #Storage failed?
 	else:
-	    return getValue(key)
-    return ''
+	    val = getValue(key)
+	    if(isinstance(val,str)):
+	        return val
+	    else:
+	        return "" #Not stored value
+    return "" #No valid request?
+
 
 
 def setValue(key,value):
-    values[key]=value
-    with open(DICTIONARY_FILE, 'wb') as handle:
-        pickle.dump(values,handle)
-        handle.close()
-    
+    sqlConnection = sqlite3.connect(DICTIONARY_FILE)
+    cursor = sqlConnection.cursor()
+    t = (key, value)
+    cursor.execute("INSERT OR REPLACE INTO val (key,val) VALUES (?,?);",t)
+    sqlConnection.commit()
+    sqlConnection.close()
+    return value
+
 def getValue(key):
-    print values
-    if key in values:
-        return values[key]
-    else:
-	return "Key Not Found"
+    sqlConnection = sqlite3.connect(DICTIONARY_FILE)
+    cursor = sqlConnection.cursor()
+    t = (key,)
+    cursor.execute("SELECT val FROM val WHERE key = ?;",t)
+    val = cursor.fetchone()
+    sqlConnection.commit()
+    sqlConnection.close()
+    if(val is not None):
+        return str(val[0])
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
